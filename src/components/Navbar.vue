80a56b8c7cb4432c78d0b7b83e8347c99e4da2a6
Francesco Sorrentino <francesco.sorrentino@studenti.unitn.it>
2024-05-02 09:39:45 +0200

    RouterLink and buttons setup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,22 @@
 <script>
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
     <v-toolbar border density="comfortable">
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Ricerca Parcheggio Efficace</v-toolbar-title>
+        <router-link to="/" style="text-decoration: none; color: inherit;">
+            <v-toolbar-title>
+                <span class="font-weight-bold capitalize">Ricerca Parcheggio Efficace</span>
+            </v-toolbar-title>
+        </router-link>
         <v-spacer></v-spacer>
-        <v-btn prepend-icon="mdi-account" variant="outlined" to="/register">
+        <v-divider vertical ></v-divider>
+        <v-btn append-icon="mdi-account" variant="text" to="/register" size="x-large">
         Register
         </v-btn>
-        <v-btn prepend-icon="mdi-login" variant="outlined" to="/login">
+        <v-divider vertical ></v-divider>
+        <v-btn append-icon="mdi-login" variant="text" to="/login" size="x-large">
         Log-in
         </v-btn>
     </v-toolbar>
