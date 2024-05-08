56f443bfb35e1fd5b2e375ee256398a9f50e91b9
Carlosorre02 <carlo.sorrentino@studenti.unitn.it>
2024-05-08 13:15:12 +0200

    aggiunta registrazione e login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="login">
-      <h1>This is a login page</h1>
+    <<div class="d-flex justify-center  mb-6">
+      <v-card flat>
+      <h1>Log-in</h1>
+      <p>Accedi al nostro servizio!</p>
+      <v-form v-on:submit.prevent="login">
+      <v-text-field label="Email" variant="outlined" v-model="email"></v-text-field>
+      <v-text-field label="Password" variant="outlined" v-model="password"></v-text-field>
+      <v-btn variant="outlined" @click="login"> Accedi</v-btn>
+      </v-form> 
+      </v-card>
     </div>
   </template>
-  
+
+  <script setup>
+  import {ref} from "vue"
+  const email=ref()
+  const password=ref()
+  function login(){
+    console.log(email.value)
+    console.log(password.value)
+  }
+  </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,26 @@
 <template>
-    <div class="register">
-      <h1>This is a register page</h1>
+    <<div class="d-flex justify-center  mb-6">
+      <v-card flat>
+      <h1>Registrazione</h1>
+      <p>Registrati al nostro servizio!</p>
+      <v-form v-on:submit.prevent="registrazione">
+      <v-text-field label="Email" variant="outlined" v-model="email"></v-text-field>
+      <v-text-field label="Password" variant="outlined" v-model="password"></v-text-field>
+      <v-text-field label="Conferma Password" variant="outlined"></v-text-field>
+      <v-checkbox label="Dichiaro di essere maggiorenne"></v-checkbox>
+      <v-btn variant="outlined" @click="registrazione"> Conferma registrazione</v-btn>
+      </v-form> 
+      </v-card>
     </div>
   </template>
+
+  <script setup>
+  import {ref} from "vue"
+  const email=ref()
+  const password=ref()
+  function registrazione(){
+    console.log(email.value)
+    console.log(password.value)
+  }
+  </script>
   
