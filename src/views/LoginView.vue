bb2f583f0d38df6afa0ab6309b583eb8a7a8482e
Francesco Sorrentino <francesco.sorrentino@studenti.unitn.it>
2024-05-27 11:29:40 +0200

    Fix login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,13 +4,13 @@
       <h1>Log-in</h1>
       <div v-if="!token">
         <div v-if="errore_login">{{ errore_login }} </div> 
-        <div v-else-if="!response_login.value"> ... </div>
+        <div v-else-if="!response_login.value"></div>
         <div v-else> Login avvenuto con successo </div>
         <p>Accedi al nostro servizio!</p>
         <v-form v-on:submit.prevent="login">
         <v-text-field label="Email" variant="outlined" v-model="email"></v-text-field>
         <v-text-field label="Password" variant="outlined" v-model="password"></v-text-field>
-        <v-btn variant="outlined" @click="login"> Accedi</v-btn>
+        <v-btn variant="outlined" @click="login">Accedi</v-btn>
         </v-form> 
       </div>
       <div v-else>
@@ -25,8 +25,8 @@
   import router from "@/router/index.js";
   import {response_login, errore_login, fetchLogin} from "../states/utente.js"
   import {onMounted, ref} from "vue"
-  const email=ref("testpren@test.com")
-  const password=ref("test")
+  const email=ref("")
+  const password=ref("")
   const token = ref()
 
   onMounted(() => {
@@ -36,6 +36,9 @@
   async function login(){
     console.log(email.value)
     console.log(password.value)
+    if(email.value == "" || password.value == ""){
+      errore_login.value = "Login fallita";
+    }
     if(email.value && password.value){
       await fetchLogin(email.value, password.value);
     }
