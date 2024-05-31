dfcc4138f3f70b72370d135cec72fb773ac03e33
Francesco Sorrentino <francesco.sorrentino@studenti.unitn.it>
2024-05-31 09:30:41 +0200

    deploy setup files

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
-  // base: process.env.BASE_PATH || '/'
-  base: process.env.NODE_ENV === 'production'
-          ? '/RPEApp/' // prod
-          : '/', // dev
 })
